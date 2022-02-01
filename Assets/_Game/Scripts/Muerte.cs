a7c7762c10f370f81e92cca5879a6d545eecc58c
Morion <adrian.martinez@udea.edu.co>
2022-02-01 15:51:38 -0500

    techo del laboratorio

--- Assets/_Game/Scripts/Muerte.cs
+++ Assets/_Game/Scripts/Muerte.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Muerte : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velAbierto;
+    public float velCerrado;
+    public float estadoInicial;
+    public float estadoActual;
+    public Transform visual;
     void Start()
     {
-        
+        estadoActual = estadoInicial;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        estadoActual -= (CambiarModo.singleton.abierto ? velAbierto : velCerrado) * Time.deltaTime;
+        visual.localScale = new Vector3(1, estadoActual / estadoInicial, 1);
+		if (estadoActual<=0)
+		{
+            SceneManager.LoadScene("GameOver");
+		}
     }
 }
